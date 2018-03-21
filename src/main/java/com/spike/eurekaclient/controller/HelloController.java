1576b2aadfa21756331863ae4c70680fc0d8132b
spike <526915701@qq.com>
2018-03-21 13:44:08 +0800

    服务生产者配置DEMO

---
+++ src/main/java/com/spike/eurekaclient/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.spike.eurekaclient.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author spike.lin
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam String name) {
+        return "hello " + name + ", this is first message";
+    }
+}
